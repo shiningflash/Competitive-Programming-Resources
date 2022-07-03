91ec0a0b4b8967a309db9036b00710e7cbd257cd
Amirul Islam <amirulislamalmamun@gmail.com>
2022-07-04 02:25:28 +0600

    quick sort added

---
+++ Sorting-Algorithms/quick_sort.py
@@ -0,0 +1,22 @@
+def quick_sort(arr):
+    if len(arr) < 2:
+        return arr
+
+    pivot = arr.pop()
+
+    lower_part = []
+    greater_part = []
+
+    for x in arr:
+        if x < pivot:
+            lower_part.append(x)
+        else:
+            greater_part.append(x)
+
+    return quick_sort(lower_part) + [pivot] + quick_sort(greater_part)
+
+
+if __name__ == '__main__':
+    arr = [5, 1, 423, 12, -1, -1231, 9, 0]
+    arr = quick_sort(arr)
+    print(arr)
